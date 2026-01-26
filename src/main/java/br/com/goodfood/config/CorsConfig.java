900eba47328213a1e082d1263e13161e2ce00a9f
Victor <victor2002gabriel@gmail.com>
2026-01-25 21:29:09 -0300

    Cors e config

--- src/main/java/br/com/goodfood/config/CorsConfig.java
+++ src/main/java/br/com/goodfood/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Alterar para url do front-end
+                .allowedOrigins("*") // Alterar para url do front-end
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
